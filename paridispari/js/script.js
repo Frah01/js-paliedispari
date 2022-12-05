9d4b62707b7989c488eb2fc6b71e80a0838bf116
Frah01 <vinario.francesco@gmail.com>
2022-12-05 19:10:17 +0100

    bonus fatto

--- paridispari/js/script.js
+++ paridispari/js/script.js
@@ -1,35 +1,52 @@
-let paridispari= prompt('Scegliere pari o dispari')
-let numero= parseInt(prompt('Scegliere un numero da 1 a 5'))
-console.log(numero)
+const buttongenerate= document.getElementById ('generate');
 
+generate.addEventListener('click', function(){
 
-function generaNumero(min,max){
-   
-    let randomNumber= Math.floor(Math.random() * (max - min + 1) + min )
-    return randomNumber
+    let input_paridispari= document.getElementById('pari_dispari');
+    let value_input= input_paridispari.value
+    console.log(value_input)
+
+    let input_number= document.getElementById('scelta_numero').value;
+    let second_value_input= input_number * 1
+    console.log (second_value_input)
     
-}
-let min = 1
-let max = 5
+    function generaNumero(min,max){
+   
+        let randomNumber= Math.floor(Math.random() * (max - min + 1) + min )
+        return randomNumber
+        
+    }
 
-let number= generaNumero(min,max);
-console.log(number)
+    let min = 1
+    let max = 5
+    
+    let number= generaNumero(min,max);
+    console.log(number)
+    
+    let somma = number + second_value_input
+    console.log(somma)
+    
+    if(somma % 2 !== 0 && input_paridispari === 'dispari'){
+        alert('Hai vinto')
+    }
+    
+    else if(somma % 2 === 0 && input_paridispari === 'pari'){
+        alert('Hai vinto')
+    }
+    
+    else {
+        alert('Hai perso')
+    }
 
-let somma = numero + number
-console.log(somma)
+})
 
-if(numero == number){
-    alert('Pareggio')
-}
 
-else if(somma % 2 === 0 && paridispari === 'pari'){
-    alert('Hai vinto')
-}
 
-else if(somma % 2 !== 0 && paridispari === 'dispari'){
-    alert('Hai vinto')
-}
 
-else {
-    alert('Hai perso')
-}
+
+
+
+
+
+
+
